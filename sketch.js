d63732fe12305c71cae1c4c1af859547e0966d61
Rodrigo Torrico Del Castillo <rodrigo.torricodelcastillo@gmail.com>
2018-05-24 13:27:49 -0400

    added movement and refactored functions

--- sketch.js
+++ sketch.js
@@ -1,24 +1,38 @@
+
+//variables
 let angle = 0;
 let train;
+let vel = 2;
+let posX = 0;
+let posY = 0;
+let posZ = -800;
+
+//size of the canvas
+const width = 900;
+const height = 700;
+
+//color
+let red = [255,0,0];
+let white = [255,255,255];
 
 function preload() {
   train = loadModel('train-corrected.obj');
 }
 
 function setup() {
-  createCanvas(400, 300, WEBGL);
+  createCanvas(width, height, WEBGL);
 }
 
 function draw() {
+  //scene
   background(0);
-  ambientLight(255, 0, 255);
-  directionalLight(255, 255, 255, 0, 0, 1);
+  ambientLight(red);
+  directionalLight(255,255,255,0,0,0,1);
+  //functions
   orbitControl();
-
-  rotateY(angle * 0.6);
-
-
-  translate(0, 0, 0);
+  //model
+  rotateY(45);
+  translate(posX, posY, (posZ += vel));
   model(train);
-  angle += 0.03;
+ 
 }
