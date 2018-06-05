2864861d3d84ff1b451859fd0d2c8ee6f1855c9f
DanAparicio22 <daparicio52@gmail.com>
2018-06-05 12:27:14 -0400

    Refactoring code and moving files into assets folder

--- sketch.js
+++ sketch.js
@@ -1,54 +1,77 @@
 //variables
-
 let train;
-let vel = 2;
-let posX = 0;
-let posY = 0;
-let posZ = -800;
-let iniciAlPos=-1200;
+var music;
+let velocity = 2;
+let positionX = -100;
+let positionY = 160;
+let positionZ = -800;
+let initialPosition = -1200;
 
 //size of the canvas
 const width = 900;
 const height = 700;
 
-//color
+//colors
 let red = [255,0,0];
-let white = [255,255,255];
 let green = [0,255,0];
-
+let blue = [0,0,255];
+let white = [255,255,255];
+let lightWhite = [255,255,255,0];
 
 function preload() {
-  train = loadModel('train-corrected.obj');
+  train = loadModel('./models/train-corrected.obj');
+  music = loadSound("./assets/sound/Thomas-The-Tank-Engine-Theme-Song.mp3");
+  wood = loadImage("./assets/textures/wood.jpg");
+  grass = loadImage("./assets/background/535811203.jpg");
 }
 
 function setup() {
   createCanvas(width, height, WEBGL);
-  wood = loadImage("./wood.jpg");
-  grass = loadImage("./grass.jpg");
+}
+
+function playBackgroundMusic() {
+    if (!music.isPlaying()) {
+      music.play();
+    }
+}
+
+function prepareScene() {
+  background(0,0,0);
+  // ambientLight(green,0,0,1);
+  directionalLight(lightWhite, 0, 0, -30);
+}
+
+function addTextureToModel(definedTexture, definedModel) {
+  texture(definedTexture);
+  model(definedModel);
+}
+
+function definePositionOfTrain() {
+  rotateY(45);
+  if(positionZ >= 420){
+    positionZ = initialPosition;
+  }
+}
+
+function moveTrain() {
+  push();
+    definePositionOfTrain();
+    translate(positionX, positionY, (positionZ += velocity));
+    addTextureToModel(wood, train);
+  pop();
+}
+
+function defineTerrain() {
+  push();
+    translate(-200,20,-725);
+    texture(grass);
+    plane(2400,2000);
+  pop();
 }
 
 function draw() {
-  //scene
-  background(color(49, 109, 237));
-  // ambientLight(green);
-  directionalLight(255,255,255,0,0,0,1);
-  push();
-
-    //model
-    rotateY(45);
-
-    if(posZ>=420){
-      posZ=iniciAlPos;
-    }
-    translate(posX, posY, (posZ += vel));
-    texture(wood);
-    model(train);
-  pop();
-  push();
-    translate(0,60,-500);
-    rotateX(90);
-    texture(grass);
-    plane(2000,1200);
-  pop();
- 
+  //playBackgroundMusic();
+  prepareScene();
+  defineTerrain();
+  moveTrain();
 }
