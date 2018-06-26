961bb0d403345c7fcd8f28a1970b61768456d0e0
JoelEche20 <39532650+JoelEche20@users.noreply.github.com>
2018-06-26 00:18:25 -0400

    humo del tren

--- sketch.js
+++ sketch.js
@@ -11,7 +11,6 @@ let autoplay = true;
 //size of the canvas
 const width = 900;
 const height = 700;
-
 //colors
 let red = [255,0,0];
 let green = [0,255,0];
@@ -19,17 +18,48 @@ let blue = [0,0,255];
 let white = [255,255,255];
 let lightWhite = [255,255,255,0];
 
+let speed=0.4;
+let posBurbble=positionZ+40;
+let a=1;
+function burbble() {
+    
+    rotateY(45);
+    texture(bubbleTexture);
+
+    push();
+    translate(positionX, positionY-110,   posBurbble -= speed  );
+    sphere(10);
+    pop();
+    push();
+    translate(positionX, positionY-140,   posBurbble -= speed  );
+    sphere(8);
+    pop();
+    push();
+    translate(positionX, positionY-155,   posBurbble -= speed  );
+    sphere(12);
+    pop();
+    push();
+    translate(positionX, positionY-167,   posBurbble -= speed  );
+    sphere(4);
+    pop();
+    a+=1;
+    if(a==12){
+      posBurbble=positionZ+40;
+      a=1;
+    }
+    
+    
+}
 function preload() {
   train = loadModel('./models/train-with-out-wheels.obj');
   music = loadSound("./assets/sound/Thomas-The-Tank-Engine-Theme-Song.mp3");
   wood = loadImage("./assets/textures/wood.jpg");
-  bubbleTexture= loadImage("./assets/textures/bubble1.jpg");
+  bubbleTexture= loadImage("./assets/textures/negro.jpg");
   grass = loadImage("./assets/background/cold.jpg");
 }
 
 function setup() {
   createCanvas(width, height, WEBGL);
-  
 }
 
 function playBackgroundMusic() {
@@ -86,20 +116,6 @@ function defineTerrain() {
   pop();
 }
 
-let a = 1;
-let b = positionZ;
-
-function bubbles()
-{
-  console.log('dibujando burbujas');
-  push();
-    rotateY(45);
-    texture(bubbleTexture);
-    translate(positionX, positionY-110,   b -= a  );
-    sphere(10);
-  pop();
-}
-
 
 function draw() {
   //playBackgroundMusic();
@@ -109,6 +125,8 @@ function draw() {
   wheel(-2, 20);
   wheel(-2, -40);
   wheel(40, 20);
-  bubbles();
+  burbble();
+  
+  
 }
 
